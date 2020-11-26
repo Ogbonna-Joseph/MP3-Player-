775771ddb94f5bf3595c4e1d3df7ff137aedbecc
Joseph Ogbonna <58666630+Ogbonna-Joseph@users.noreply.github.com>
2020-11-25 18:59:14 -0800

    codebase_mp3_ui_design

---
+++ main.py
@@ -0,0 +1,22 @@
+from pygame import mixer
+
+mixer.init()
+
+mixer.music.load("jara.mp3")
+mixer.music.set_volume(0.8)
+mixer.music.play()
+
+while True:
+    print("press p to pause r to resume")
+    print("press e to exit the program")
+
+    query = input('')
+
+    if query == 'p':
+        mixer.music.pause()
+    elif query == 'r':
+        mixer.music.unpause()
+    elif query == 'e':
+        mixer.music.stop()
+        break
+
